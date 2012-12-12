d50a52e107bc927abb7a53d0f5b20e4e7b77fd82
Mo Mughrabi <mo.mughrabi@gmail.com>
2012-12-12 20:12:24 +0300

    populate setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,10 @@
-import os
 from setuptools import setup, find_packages
-
-f = open(os.path.join(os.path.dirname(__file__), 'README.txt'))
-readme = f.read()
-f.close()
 
 
 setup(
     name='django-google-translator',
     version='0.1',
     description='django-google-translator is a reusable Django application for translating .PO files through Google API.',
-    long_description=readme,
     author='Mo Mughrabi',
     author_email='mo.mughrabi@gmail.com',
     url='https://github.com/mo-mughrabi/django-google-translator/tree/master',
